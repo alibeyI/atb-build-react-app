d83f48aad97026071e1ba544120080cb47979bc6
alibeyI <alibey.ibrahimli@gmail.com>
2020-05-14 12:28:54 +0400

    Updates

---
+++ precache-manifest.bf792bcd21d5ac2c613ea3802220c913.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ba4bb9f414782d0ef93ab7053134a9",
+    "url": "/atb-build-react-app/index.html"
+  },
+  {
+    "revision": "78af1470f571242a9f0e",
+    "url": "/atb-build-react-app/static/css/main.29cfee7e.chunk.css"
+  },
+  {
+    "revision": "fe588e7b0d04f1a79968",
+    "url": "/atb-build-react-app/static/js/2.09c24cc5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/atb-build-react-app/static/js/2.09c24cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78af1470f571242a9f0e",
+    "url": "/atb-build-react-app/static/js/main.32eb8cc4.chunk.js"
+  },
+  {
+    "revision": "ee6a222f058377f41607",
+    "url": "/atb-build-react-app/static/js/runtime-main.bf64dba4.js"
+  },
+  {
+    "revision": "b1bfca5e7deffb476705acd3af75426b",
+    "url": "/atb-build-react-app/static/media/logoAtb.b1bfca5e.svg"
+  },
+  {
+    "revision": "99dc552ebc2a69c7a708561191efdb75",
+    "url": "/atb-build-react-app/static/media/maestro.99dc552e.svg"
+  },
+  {
+    "revision": "cd93a85a258966bc34eee9f0714d4e01",
+    "url": "/atb-build-react-app/static/media/master.cd93a85a.svg"
+  },
+  {
+    "revision": "f6931727ef068a93674a26fb468556ed",
+    "url": "/atb-build-react-app/static/media/num.f6931727.svg"
+  },
+  {
+    "revision": "4dabd9f4523fca4b92dc0a2b360dd6e8",
+    "url": "/atb-build-react-app/static/media/recipeIcon.4dabd9f4.svg"
+  },
+  {
+    "revision": "16ed809c729a7e2d304d311dc57accfa",
+    "url": "/atb-build-react-app/static/media/visa.16ed809c.svg"
+  },
+  {
+    "revision": "d3ef3dc56c3ed4e848a3cd4b078e4c50",
+    "url": "/atb-build-react-app/static/media/visa2.d3ef3dc5.svg"
+  }
+]);
