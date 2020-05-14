7e13c688bba3741fc31a258b0ec2ae317ff826c0
alibeyI <alibey.ibrahimli@gmail.com>
2020-05-14 12:27:54 +0400

    react-app-build

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import Recipe from './components/Recipe';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Recipe />
+    </div>
+  )
+}
+
+export default App;
